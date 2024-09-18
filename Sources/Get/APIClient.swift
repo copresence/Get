88dcbe3bd04f1b2246be9d3600221aaa63f63d1c
Stephen O'Connor <oconnor.freelance@gmail.com>
2024-09-18 08:37:38 +0200

    change pretty print implementation.

--- Sources/Get/APIClient.swift
+++ Sources/Get/APIClient.swift
@@ -429,13 +429,15 @@ public enum APIError: Error, LocalizedError {
 extension Data {
     
     /// NSString gives us a nice sanitized debugDescription
-    var prettyPrintedJSONString: NSString? {
-        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
-              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
-              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else {
+    var prettyPrintedJSONString: String? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []) else {
             return nil
         }
-
+        guard let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted, .sortedKeys]) else {
+            return nil
+        }
+        
+        let prettyPrintedString = String(decoding: data, as: UTF8.self)
         return prettyPrintedString
     }
 }
